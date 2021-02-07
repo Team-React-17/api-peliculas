0d32461079da9d757d232c04667cd1660f34a527
David Fabian <david.fabian@phinxlab.com>
2021-02-07 20:56:17 -0300

    adding material theme

---
+++ src/theme/theme.ts
@@ -0,0 +1,27 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeOptions } from '@material-ui/core/styles/createMuiTheme';
+
+export const themeOptions: ThemeOptions | any = {
+  palette: {
+    type: 'dark',
+    primary: {
+      main: '#22223b',
+      light: 'rgb(23, 23, 41)',
+    },
+    secondary: {
+      main: '#4a4e69',
+    },
+    background: {
+      default: 'rgb(23, 23, 41)',
+      paper: '#dee2e6',
+    },
+    text: {
+      primary: '#4a4e69',
+      secondary: '#c1c4de',
+      disabled: '#8b8ea6',
+      hint: '#b8bbd6',
+    },
+  },
+};
+
+export const theme = createMuiTheme(themeOptions);
