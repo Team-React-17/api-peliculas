bd47e04efc07fcd5c128619ca99ebe6ace11e0f1
David Fabian <david.fabian@phinxlab.com>
2021-02-07 23:34:29 -0300

    Adding Section

---
+++ src/components/Section/Section.tsx
@@ -0,0 +1,31 @@
+import React, { FunctionComponent } from 'react';
+import { Container, Divider, Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  container: {
+    marginTop: 20,
+    paddingTop: 20,
+    paddingBottom: 20,
+  },
+  divider: {
+    marginBottom: 20,
+  }
+});
+
+interface OwnProps {
+  title: string;
+}
+
+type Props = OwnProps;
+
+export const Section: FunctionComponent<Props> = ({ children, title }) => {
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <Typography variant="h4">{title}</Typography>
+      <Divider className={classes.divider}/>
+      {children}
+    </Container>
+  );
+};
